117189a8edd661d75508ae09b3f63f9ba8e12ea2
Ridalord <ridhwan.alabi@gmail.com>
2023-05-15 18:09:33 +0100

    Add menu overlay

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const menu = document.querySelector('.menu-button');
-const navLinks = document.querySelector('.menuLinks')
-menu.addEventListener('click', (e) => {
-  e.preventDefault();
-  if (navLinks.style.display == "none") {
-    navLinks.style.display = "block"
-  } else {
-    navLinks.style.display = "none"
-  }
-})
+const menu = document.querySelectorAll('.menu-button');
+const navLinks = document.querySelector('.nav-links');
+const cancel = document.querySelector('.link-cancel');
+menu.forEach((menuBtn) => {
+  menuBtn.addEventListener('click', () => {
+    if (navLinks.classList.contains('menuLinks')) {
+      navLinks.classList.remove('menuLinks');
+      cancel.classList.remove('d-none');
+    } else {
+      navLinks.classList.add('menuLinks');
+      cancel.classList.add('d-none');
+    }
+  });
+});

10dff1240260ba6d2385bbe1f4b9d61827ea2c97
Ridalord <ridhwan.alabi@gmail.com>
2023-05-16 18:14:16 +0100

    Eslint error fixed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const menu = document.querySelectorAll('.menu-button');
 const navLinks = document.querySelector('.nav-links');
 const cancel = document.querySelector('.link-cancel');
-const portfolioItems = document.querySelectorAll('.portfolio-details')
+const portfolioItems = document.querySelectorAll('.portfolio-details');
 const wrapper = document.querySelector('.wrapper');
 menu.forEach((menuBtn) => {
   menuBtn.addEventListener('click', () => {
@@ -17,67 +17,63 @@ menu.forEach((menuBtn) => {
 
 const portfolioDetails = [
   {
-    id: "1",
-    name: "Tonic",
+    id: '1',
+    name: 'Tonic',
     year: '2015',
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    title: "CANOPY",
-    stack: "Backend Dev",
+    title: 'CANOPY',
+    stack: 'Backend Dev',
     workStack: ['HTML', 'CSS', 'JAVASCRIPT'],
-    year: "2015",
-    img: "img/nature.png",
-    source: "#",
-    live: "#"
+    img: 'img/nature.png',
+    source: '#',
+    live: '#',
   },
   {
-    id: "2",
-    name: "Multi-Post Stories",
+    id: '2',
+    name: 'Multi-Post Stories',
     year: '2015',
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    title: "FACEBOOK",
-    stack: "Full Stack Dev",
+    title: 'FACEBOOK',
+    stack: 'Full Stack Dev',
     workStack: ['HTML', 'Ruby on Rails', 'CSS', 'JAVASCRIPT'],
-    year: "2015",
-    img: "img/professional-art.png",
-    source: "#",
-    live: "#"
+    img: 'img/professional-art.png',
+    source: '#',
+    live: '#',
   },
   {
-    id: "3",
-    name: "Facebook 360",
+    id: '3',
+    name: 'Facebook 360',
     year: '2015',
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    title: "Facebook",
-    stack: "Full Stack Dev",
+    title: 'Facebook',
+    stack: 'Full Stack Dev',
     workStack: ['HTML', 'Ruby on Rails', 'CSS', 'JAVASCRIPT'],
-    year: "2015",
-    img: "img/facebook360.png",
-    source: "#",
-    live: "#"
+    img: 'img/facebook360.png',
+    source: '#',
+    live: '#',
   },
   {
-    id: "4",
-    name: "Uber Navigation",
+    id: '4',
+    name: 'Uber Navigation',
     year: '2015',
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    title: "Uber",
-    stack: "Lead Developer",
+    title: 'Uber',
+    stack: 'Lead Developer',
     workStack: ['HTML', 'Ruby on Rails', 'CSS', 'JAVASCRIPT'],
-    year: "2015",
-    img: "img/Availability.png",
-    source: "#",
-    live: "#"
+    img: 'img/Availability.png',
+    source: '#',
+    live: '#',
   },
-]
+];
 
 if (portfolioItems) {
   portfolioItems.forEach((cards) => {
     cards.addEventListener('click', (e) => {
       const idValue = e.target.closest('.tonic').id;
-      for (var i = 0; i < portfolioDetails.length; i++){
-        if (portfolioDetails[i].id == idValue) { 
-          const popupSection = document.createElement('section')
-          popupSection.classList.add("popup-section");
+      for (let i = 0; i < portfolioDetails.length; i += 1) {
+        if (portfolioDetails[i].id === idValue) {
+          const popupSection = document.createElement('section');
+          popupSection.classList.add('popup-section');
           popupSection.innerHTML = `
             <div class="background"></div>
             <div class="tonic d-flex">
@@ -90,7 +86,7 @@ if (portfolioItems) {
                 <li><img src="img/Counter.png" alt="Counter"></li>
                 <li>${portfolioDetails[i].stack}</li>
                 <li><img src="img/Counter.png" alt="Counter"></li>
-                <li>${portfolioDetails[i].year}</li>
+                <li>2015</li>
               </ul>
               <div class="works-image">
                 <img src="${portfolioDetails[i].img}" alt="${portfolioDetails[i].name} Snapshoot">
@@ -106,15 +102,14 @@ if (portfolioItems) {
                 </div>
               </div>
             </div>
-          `
-          wrapper.appendChild(popupSection)
+          `;
+          wrapper.appendChild(popupSection);
           const closeButton = popupSection.querySelector('.close');
           closeButton.addEventListener('click', () => {
             wrapper.removeChild(popupSection);
           });
         }
       }
-    })
-  })
+    });
+  });
 }
-

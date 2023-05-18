49ba36d476b526600e47347c89452a20a66bc86a
Ridalord <ridhwan.alabi@gmail.com>
2023-05-18 12:11:46 +0100

    Add contact validation

--- script.js
+++ script.js
@@ -147,3 +147,37 @@ if (portfolioItems) {
     });
   });
 }
+
+const contactForm = document.getElementById('contactForm');
+const email = document.getElementById("mail");
+const emailError = document.querySelector("#mail + span.errorContainer");
+
+email.addEventListener("input", () => {
+  if (email.validity.valid) {
+    emailError.textContent = "";
+    emailError.className = "error"; 
+  } else {
+    showError();
+  }
+});
+contactForm.addEventListener('submit', (e) => {
+  if (!email.validity.valid) {
+    showError();
+    e.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    emailError.textContent = "You need to enter an email address.";
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = "Entered value needs to be an email address.";
+  } else if (email.validity.tooShort) {
+    emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+  } else if (email.validity.patternMismatch) {
+    emailError.textContent = `Please enter a valid email address with lowercase letters`;
+  }
+  emailError.className = "error active";
+}
+
+

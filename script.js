014b4ceec95630c315afeae7bdbbd82ea832dea0
Ridalord <ridhwan.alabi@gmail.com>
2023-05-18 02:07:08 +0100

    Fixed review errors

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ menu.forEach((menuBtn) => {
 
 const portfolioDetails = [
   {
-    id: '1',
+    id: '0',
     name: 'Tonic',
     year: '2015',
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
@@ -29,7 +29,7 @@ const portfolioDetails = [
     live: 'https://ridalord.github.io/my_portfolio/',
   },
   {
-    id: '2',
+    id: '1',
     name: 'Multi-Post Stories',
     year: '2015',
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
@@ -41,7 +41,7 @@ const portfolioDetails = [
     live: 'https://ridalord.github.io/my_portfolio/',
   },
   {
-    id: '3',
+    id: '2',
     name: 'Facebook 360',
     year: '2015',
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
@@ -53,7 +53,7 @@ const portfolioDetails = [
     live: 'https://ridalord.github.io/my_portfolio/',
   },
   {
-    id: '4',
+    id: '3',
     name: 'Uber Navigation',
     year: '2015',
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
@@ -96,52 +96,54 @@ if (portfolio) {
       </div>
     `;
     portfolio.appendChild(card);
-    const portfolioItems = document.querySelectorAll('.portfolio-details');
-    if (portfolioItems) {
-      portfolioItems.forEach((cards) => {
-        cards.addEventListener('click', (e) => {
-          const idValue = e.target.closest('.tonic').id;
-          for (let i = 0; i < portfolioDetails.length; i += 1) {
-            if (portfolioDetails[i].id === idValue) {
-              const popupSection = document.createElement('section');
-              popupSection.classList.add('popup-section');
-              popupSection.innerHTML = `
-            <div class="background"></div>
-            <div class="tonic d-flex">
-              <button class="close" type="submit">
-                <i class="bi bi-x"></i>              
-              </button>
-              <h2 class="works-header">${portfolioDetails[i].name}</h2>
-              <ul class="header-sub d-flex">
-                <li class="main-header">${portfolioDetails[i].title}</li>
-                <li><img src="img/Counter.png" alt="Counter"></li>
-                <li>${portfolioDetails[i].stack}</li>
-                <li><img src="img/Counter.png" alt="Counter"></li>
-                <li>2015</li>
-              </ul>
-              <div class="works-image" style="background-image: url(${portfolioDetails[i].img});">
-              </div>
-              <div class="details d-flex">
-                <p class="works-text">${portfolioDetails[i].text}</p>
-                <ul class="works-stack d-flex">
-                  ${portfolioDetails[i].workStack.map((stack) => `<li>${stack}</li>`).join('')}
+  }
+}
+
+const portfolioItems = document.querySelectorAll('.portfolio-details');
+if (portfolioItems) {
+  portfolioItems.forEach((cards) => {
+    cards.addEventListener('click', (e) => {
+      e.preventDefault();
+      const idValue = e.target.closest('.tonic').id;
+      for (let i = 0; i < portfolioDetails.length; i += 1) {
+        if (portfolioDetails[i].id === idValue) {
+          const popupSection = document.createElement('section');
+          popupSection.classList.add('popup-section');
+          popupSection.innerHTML = `
+              <div class="background"></div>
+              <div class="tonic d-flex">
+                <button class="close" type="submit">
+                  <i class="bi bi-x"></i>              
+                </button>
+                <h2 class="works-header">${portfolioDetails[i].name}</h2>
+                <ul class="header-sub d-flex">
+                  <li class="main-header">${portfolioDetails[i].title}</li>
+                  <li><img src="img/Counter.png" alt="Counter"></li>
+                  <li>${portfolioDetails[i].stack}</li>
+                  <li><img src="img/Counter.png" alt="Counter"></li>
+                  <li>2015</li>
                 </ul>
-                <div class="d-flex justify-content-between link-buttons">
-                  <button class="works-button"><a href="${portfolioDetails[i].live}">See Live <img src="img/live.png" alt="Live Link"/></a></button>
-                  <button class="works-button"><a href="${portfolioDetails[i].source}">See Source <img src="img/git.png" alt="Source Link"/></a></button>
+                <div class="works-image" style="background-image: url(${portfolioDetails[i].img});">
+                </div>
+                <div class="details d-flex">
+                  <p class="works-text">${portfolioDetails[i].text}</p>
+                  <ul class="works-stack d-flex">
+                    ${portfolioDetails[i].workStack.map((stack) => `<li>${stack}</li>`).join('')}
+                  </ul>
+                  <div class="d-flex justify-content-between link-buttons">
+                    <button class="works-button"><a href="${portfolioDetails[i].live}">See Live <img src="img/live.png" alt="Live Link"/></a></button>
+                    <button class="works-button"><a href="${portfolioDetails[i].source}">See Source <img src="img/git.png" alt="Source Link"/></a></button>
+                  </div>
                 </div>
               </div>
-            </div>
-          `;
-              wrapper.appendChild(popupSection);
-              const closeButton = popupSection.querySelector('.close');
-              closeButton.addEventListener('click', () => {
-                wrapper.removeChild(popupSection);
-              });
-            }
-          }
-        });
-      });
-    }
-  }
+            `;
+          wrapper.appendChild(popupSection);
+          const closeButton = popupSection.querySelector('.close');
+          closeButton.addEventListener('click', () => {
+            wrapper.removeChild(popupSection);
+          });
+        }
+      }
+    });
+  });
 }
